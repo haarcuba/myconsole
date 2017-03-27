1217495de2f50e54d0005e864f12b19277fa47a1
Yoav Kleinberger <haarcuba@gmail.com>
2017-03-27 16:32:58 +0300

    version 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README = 'create IPython consoles with custom banners'
 requires = [ 'ipython' ]
 
 setup(name='myconsole',
-      version='0.1.0',
+      version='1.0.0',
       description=README,
       long_description=README,
       url='https://github.com/haarcuba/myconsole',
